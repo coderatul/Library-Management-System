e1c0f977227963b377746f23d585f290b62a0546
Atul Kushwaha <developeratulkushwaha@gmail.com>
2021-10-22 14:01:51 +0530

    comments added

--- Main - Library Management System/Library Management System/AddBook.py
+++ Main - Library Management System/Library Management System/AddBook.py
@@ -1,21 +1,22 @@
+#function to add Book code
 def add_code(p):
     n = open("Book_code.txt", "a")
     n.write(p+"\n")
     n.close()
 
-
+#function to add Book name
 def add_book(p):
     n = open("Books.txt", "a")
     n.write(p+"\n")
     n.close()
 
-
+#function to add publish year 
 def add_pub_year(p):
     n = open("Publish_year.txt", "a")
     n.write(p+"\n")
     n.close()
 
-
+#function to add Author name
 def add_auth(p):
     n = open("Author_name.txt", "a")
     n.write(p+"\n")
